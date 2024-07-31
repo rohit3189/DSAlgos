ecb3a2d08e16a2bffb54b9f9c76c842a068938f4
rohit0599 <rohit.parida@wissen.com>
2024-07-31 18:08:12 +0530

    @ParameterizedTest code refactored

--- src/test/java/com/excercise/DSAlgo/Q6Test.java
+++ src/test/java/com/excercise/DSAlgo/Q6Test.java
@@ -3,27 +3,27 @@ package com.excercise.DSAlgo;
 import common_code.LinkedList;
 import common_code.LinkedListNode;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class Q6Test {
 
-    @Test
-    public void testFindMiddle_EvenLength() {
-        int[] evenLengthArray = {5, 7, 2, 4, 3, 6};
+    @ParameterizedTest
+    @MethodSource("streamOfArrays")
+    public void testFindMiddle(int[] arr) {
         LinkedList linkedList = new LinkedList();
-        linkedList.createLinkedList(evenLengthArray);
+        linkedList.createLinkedList(arr);
         LinkedListNode linkedListNode = Q6.findMiddle(linkedList.head);
-
-        Assertions.assertEquals(4, linkedListNode.data);
+        if (arr.length % 2 == 0) {
+            Assertions.assertEquals(4, linkedListNode.data);
+        } else {
+            Assertions.assertEquals(12, linkedListNode.data);
+        }
     }
 
-    @Test
-    public void testFindMiddle_OddLength() {
-        int[] oddLengthArray = {6, 9, 7, 12, 43, 5, 11};
-        LinkedList linkedList = new LinkedList();
-        linkedList.createLinkedList(oddLengthArray);
-        LinkedListNode linkedListNode = Q6.findMiddle(linkedList.head);
-
-        Assertions.assertEquals(12, linkedListNode.data);
+    private static Stream<int[]> streamOfArrays() {
+        return Stream.of(new int[]{5, 7, 2, 4, 3, 6}, new int[]{6, 9, 7, 12, 43, 5, 11});
     }
 }
